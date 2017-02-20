d018924bfcc604479ffec72055ce902bf1b68c2d
Kenneth C. LaMarca <klamarca@chessmh.com>
2017-02-19 22:23:31 -0500

    Added email servicing to server.js , TODO:  test and write found end implementation in contactMe.js

--- Server.js
+++ Server.js
@@ -8,6 +8,7 @@ var app = express();
 var router = express.Router();
 var temp =  __dirname + '/pages/';
 var path = require("path");
+var nodemailer = require('nodemailer');
 
 router.use(function (req, res, next) {
     console.log("/" + req.method);
@@ -36,5 +37,38 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
+    
+//EMAIL handler
+    
+    router.post('/',handleEmail);
+    var testText = "Hello my name is Server.js";
+    
+    var mailOptions = {
+        from: 'legateWebsite@gmail.com',
+        to: 'kennywu1005@gmail.com',
+        subject: 'Test Email',
+        text: testText        
+    }
+    
+    function handleEmail(req, res) {
+    // Not the movie transporter!
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'legateWebsite@gmail.com', // Your email id
+                pass: 'PsuedoLogIn!' // Your password
+            }
+        });       
+    
+        transporter.sendMail(mailOptions,function(error,info){
+            if(error){
+                console.log(error);
+                res.json({yo: 'error'});
+            }else{
+                console.log('Message sent: ' + info.response);
+                res.json({yo: info.response});
+            };        
+        });
+    }
 });
 
